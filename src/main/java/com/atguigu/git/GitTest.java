827546f07f638ae2bea51b9555ce25d2f2866922
Panjinsheng <874617225@qq.com>
2022-03-15 23:18:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author panjinsheng
+ * @create 2022-03-15 23:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
